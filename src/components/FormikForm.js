5dbefee5d69f7c315ccb11bad3ab40cdc6a94b57
reddrubyy <cynthiaoluoma2001@gmail.com>
2024-09-06 23:25:10 +0100

    Initial commit with form handling using controlled components and Formik

---
+++ src/components/FormikForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('Invalid email').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+const FormikForm = () => {
+  const handleSubmit = (values) => {
+    console.log('Form data', values);
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        username: '',
+        email: '',
+        password: '',
+      }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <div>
+          <label>Username</label>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" component="span" />
+        </div>
+        <div>
+          <label>Email</label>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="span" />
+        </div>
+        <div>
+          <label>Password</label>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="span" />
+        </div>
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FormikForm;
---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FormikForm from './components/FormikForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Registration Form with Formik</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
